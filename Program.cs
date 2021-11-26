d7dd5a640f631a9aaf362abf84110168e1951524
ayesha-sattar <aishachaudhary816@gmail.com>
2021-11-26 12:22:00 +0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CS_409
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 20, y = 35;
+            x = y++ + x++;
+            y = ++y + x++;
+            Console.WriteLine(x);
+        }
+    }
+}
